7e1b5eed4170fa925213c7e873955d4c2370c8e3
edevzi <edevzi@github.com>
2026-05-29 00:12:42 +0500

    fix: add @vercel/blob to serverComponentsExternalPackages to fix webpack undici build error

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['@vercel/blob'],
+  },
+};
 
 export default nextConfig;
